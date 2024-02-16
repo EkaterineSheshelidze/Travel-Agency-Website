54f7d5fc20f7c7fa565a1b83572a171c1fd330bf
Kato Sheshelidze <katosheshelidze@gmail.com>
2024-02-16 21:30:31 +0400

    travel agency

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+$('#owl-carousel-1').owlCarousel({
+    loop:true,
+    margin:20,
+    nav:true,
+    navText: ["<div><span>PREV</span><img src='assets/images/simple-right-arrow.png'></div>","<div><span>NEXT</span><img src='assets/images/simple-right-arrow.png'></div>"],
+    dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        768:{
+            items:2
+        },
+    }
+})
+
+$('#owl-carousel-2').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    dots:false,
+    navText: ["<div><span>PREV</span><img src='assets/images/simple-right-arrow.png'></div>","<div><span>NEXT</span><img src='assets/images/simple-right-arrow.png'></div>"],
+    responsive:{
+        768:{
+            items:1
+        },
+    }
+})
+
+document.getElementById("burger-menu").addEventListener("click", function(){
+    document.getElementById("resp-menu").classList.add("active")
+});
+document.getElementById("close").addEventListener("click", function(){
+    document.getElementById("resp-menu").classList.remove("active")
+});
